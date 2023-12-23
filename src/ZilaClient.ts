18d157d79ec599e8e386d51f593e0f2f1354c924
WarstekHUN <buug@proc.hu>
2023-12-23 13:24:48 +0100

    Max waiting time for waiters

--- src/ZilaClient.ts
+++ src/ZilaClient.ts
@@ -139,22 +139,71 @@ export default class ZilaClient {
 
   /**
    * Calls an eventhandler on the clientside for the specified client. Gets a value of T type back from the client or just waits for the eventhandler to finish.
+   * The max waiting time is the server's maxWaiterTime
    * @param {string} identifier The callback's name on the clientside.
    * @param {any|undefined} data Arguments that shall be passed to the callback as parameters (optional)
-   * @returns {Promise<unknown>}
+   * @returns {Promise<T | undefined>}
    */
-  public waiter(identifier: string, ...data: any[]): Promise<unknown> {
-    return new Promise((resolve) => {
+  public waiter<T>(identifier: string, ...data: any[]): Promise<T | undefined> {
+    return new Promise(async (resolve) => {
       const uuid = randomUUID();
+     
+      let timeout: NodeJS.Timeout;
 
-      this.setMessageHandler(uuid, (args: any[]): void => {
-        this.removeMessageHandler(uuid);
-        resolve(args);
-      });
+      resolve(
+        Promise.any([
+          new Promise(r => {
+            this.setMessageHandler(uuid, (args: any[]): void => {
+              clearTimeout(timeout);
+              this.removeMessageHandler(uuid);
+              r(args);
+              });
+          }),
+          new Promise((_r, rej) => {
+            timeout = setTimeout(() => {
+              _r(undefined);
+            }, this.server.maxWaiterTime);
+          })
+        ]) as Promise<T | undefined>
+      );
 
       this.socket.send(this.getMessageJSON(identifier, data, uuid));
     });
   }
+
+    /**
+   * Calls an eventhandler on the clientside for the specified client. Gets a value of T type back from the client or just waits for the eventhandler to finish.
+   * @param {string} identifier The callback's name on the clientside.
+   * @param {number} maxWaitingTime The maximum time this waiter will wait for the client.
+   * @param {any|undefined} data Arguments that shall be passed to the callback as parameters (optional)
+   * @returns {Promise<T | undefined>}
+   */
+    public waiterTimeout<T>(identifier: string, maxWaitingTime: number, ...data: any[]): Promise<T | undefined> {
+      return new Promise(async (resolve) => {
+        const uuid = randomUUID();
+       
+        let timeout: NodeJS.Timeout;
+  
+        resolve(
+          Promise.any([
+            new Promise(r => {
+              this.setMessageHandler(uuid, (args: any[]): void => {
+                clearTimeout(timeout);
+                this.removeMessageHandler(uuid);
+                r(args);
+                });
+            }),
+            new Promise((_r, rej) => {
+              timeout = setTimeout(() => {
+                _r(undefined);
+              }, maxWaitingTime);
+            })
+          ]) as Promise<T | undefined>
+        );
+  
+        this.socket.send(this.getMessageJSON(identifier, data, uuid));
+      });
+    }
 
   /**
    * Registers an eventhandler.
